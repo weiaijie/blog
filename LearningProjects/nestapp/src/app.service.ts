bb471aea98c86c856ddf1359bef67cbdd35c4493
weiaijie <weiaijie@Outlook.com>
2023-09-22 00:12:50 +0800

    调试

--- LearningProjects/nestapp/src/app.service.ts
+++ LearningProjects/nestapp/src/app.service.ts
@@ -1,8 +1,15 @@
+/*
+ * @Description: 
+ * @Author: xuhui
+ * @Date: 2023-09-21 23:36:48
+ * @LastEditors: weiaijie
+ * @FilePath: \blog\LearningProjects\nestapp\src\app.service.ts
+ */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World1111!';
   }
 }
